ac9833f6f80bf29f9d0610c391a27cafdea77792
KevCasHSC <castrok.c11@hotmail.com>
2020-04-21 22:52:52 +0000

    last upload

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Starting a flask web application
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Printing a message"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run('host=0.0.0.0', port=5000)
