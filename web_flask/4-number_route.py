6b0b001591c084ecef18d73651f7102aa7d4cf1a
KevCasHSC <castrok.c11@hotmail.com>
2020-04-22 17:09:20 +0000

    last upload

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def third_tab(text):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<int: n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def four_tab(n):
     """Four tab"""
     if type(n) is int:
