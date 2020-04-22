e05aaa7c042f6727c49dabc0cfafca68140ba079
KevCasHSC <castrok.c11@hotmail.com>
2020-04-22 15:22:33 +0000

    last upload

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,7 +24,7 @@ def first_tab():
 @app.route('/c/<text>', strict_slashes=False)
 def second_tab(text):
     """Second tab"""
-    return 'C {}'.format(text.replace('_'' '))
+    return 'C {}'.format(text.replace('_',' '))
 
 
 if __name__ == '__main__':
